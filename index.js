6aa67d5a87980d4eba855ad8519dee867720ed81
Joy Huang <joyhuangg@gmail.com>
2018-07-18 03:30:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function theBeatlesPlay(musicians, instruments){
   let array = []
   for (let i = 0; i < musicians.length; i++){
-    string = musicians[i] + " plays " + instruments[i]
+    let string = musicians[i] + " plays " + instruments[i]
     array.push(string)
   }
   return array
